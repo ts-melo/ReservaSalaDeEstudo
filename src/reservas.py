1c5fb91c2d359aee8a6de1b7a54c8a4fbf0534e4
RafaelKenzoUnifesp <rafael.kenzo@unifesp.br>
2026-05-11 16:06:35 -0300

    fix: mescla orberver.py e reservas.py para corrigir classe reserva duplicada

--- src/reservas.py
+++ src/reservas.py
@@ -1,8 +1,20 @@
+from observer import Observavel
 
-class Reserva:
+class Reserva(Observavel):
     def __init__(self, sala, usuario, inicio, fim):
+        super().__init__()
         self.sala = sala
         self.usuario = usuario
         self.inicio = inicio
         self.fim = fim
-        self.tipo_usuario = usuario.tipo_usuario
+        self.tipo_usuario = usuario.tipo_usuario  
+        self.status = "confirmada"             
+
+    def cancelar(self):
+        self.status = "cancelada"
+        self.notificar("cancelamento", {"status": self.status, "reserva": self})
+
+    def modificar(self, novo_inicio, novo_fim):
+        self.inicio = novo_inicio
+        self.fim = novo_fim
+        self.notificar("modificacao", {"inicio": novo_inicio, "fim": novo_fim, "reserva": self})
